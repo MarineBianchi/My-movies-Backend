f14da4bcc75f4feef005ee3d54c612dce91aa25e
Marine Bianchi <marine.bianchi@gmail.com>
2023-07-12 09:37:00 +0200

    maaaaj

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const fetch = require('node-fetch');
 let API_KEY = process.env.API_KEY;
 
 router.get('/movies', (req, res) => {
-  console.log("Keykey", API_KEY)
+  // console.log("Keykey", API_KEY)
 console.log("ma route")
     fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}`)
     .then(response => response.json())
